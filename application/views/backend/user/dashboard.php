52ab0706eb8fcbf5b949442b6810e31e5a7501b3
bautiexposito <bautistaexposito@outlook.com>
2025-04-03 23:20:32 -0300

    Cleaning up the user dashboard

--- application/views/backend/user/dashboard.php
+++ application/views/backend/user/dashboard.php
@@ -1,1 +1,104 @@
-Welcome to User Dashboard.
+<div class="main-content">
+        <section class="section">
+          <h1 class="section-header">
+            <div>Dashboard</div>
+          </h1>
+          <div class="row">
+            <div class="col-lg-3 col-md-6 col-12">
+              <div class="card card-sm-3">
+                <div class="card-icon bg-primary">
+                <i class="fa fa-dollar-sign"></i>
+                </div>
+                <div class="card-wrap">
+                  <div class="card-header">
+                    <h4>USD Balance</h4>
+                  </div>
+                  <div class="card-body">
+                    <i class="fa fa-dollar-sign"></i>
+                   4000
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-12">
+              <div class="card card-sm-3">
+                <div class="card-icon bg-danger">
+                  <i class="fa fa-paper"></i>
+                </div>
+                <div class="card-wrap">
+                  <div class="card-header">
+                    <h4>ARS Balance</h4>
+                  </div>
+                  <div class="card-body">
+                  <i class="fa fa-dollar-sign"></i>
+                8000
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-12">
+              <div class="card card-sm-3">
+                <div class="card-icon bg-warning">
+                  <i class="ion ion-paper-airplane"></i>
+                </div>
+                <div class="card-wrap">
+                  <div class="card-header">
+                    <h4>Total Votes</h4>
+                  </div>
+                  <div class="card-body">
+                  1256
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-12">
+              <div class="card card-sm-3">
+                <div class="card-icon bg-success">
+                  <i class="ion ion-record"></i>
+                </div>
+                <div class="card-wrap">
+                  <div class="card-header">
+                    <h4>Contestants</h4>
+                  </div>
+                  <div class="card-body">
+                4567
+                  </div>
+                </div>
+              </div>
+            </div>                  
+          </div>
+          <div class="row">
+            
+            <div class="col-lg-12 col-md-12 col-12 col-sm-12">
+              <div class="card">
+           
+                <div class="card-header">
+                  <h4>List of Contestants</h4>
+                </div>
+                <div class="card-body">             
+                  <ul class="list-unstyled list-unstyled-border">
+                    <!-- Get all contestants -->
+
+                 
+                    <li class="media">
+                      <img class="mr-3 rounded-circle" width="50" src="" alt="avatar">
+                      <div class="media-body">
+                        <div class="float-right">
+                          <small class="badge badge-warning">
+                          1234
+                        </small> 
+                        <small><a href="#" class="btn btn-primary btn-sm ">Vote</a></small></div>
+                        <div class="media-title">John Doe</div>
+                        <small>i am contestant number one</small>
+                      </div>
+                    </li>
+                  
+                  </ul>
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+          </div>
+        </section>
+      </div>
--- application/views/backend/user/navigation.php
+++ application/views/backend/user/navigation.php
@@ -1,20 +1,51 @@
-            <div class="main-sidebar sidebar-style-2">
-                <aside id="sidebar-wrapper">
-                    <div class="sidebar-brand">
-                        <a href="index.html">Stisla</a>
-                    </div>
-                    <div class="sidebar-brand sidebar-brand-sm">
-                        <a href="index.html">St</a>
-                    </div>
-                    <ul class="sidebar-menu">
-                        <li class="menu-header">Dashboard</li>
-                        <li class="dropdown active">
-                        <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
-                        <ul class="dropdown-menu">
-                            <li class=active><a class="nav-link" href="index-0.html">General Dashboard</a></li>
-                            <li><a class="nav-link" href="index.html">Ecommerce Dashboard</a></li>
-                        </ul>
-                        </li>
-                    </ul>
-                </aside>
+<div class="main-sidebar">
+        <aside id="sidebar-wrapper">
+          <div class="sidebar-brand">
+            <a href="index.html">Voting Software</a>
+          </div>
+          <div class="sidebar-user">
+            <div class="sidebar-user-picture">
+              <img alt="user-image" src="">
             </div>
+            <div class="sidebar-user-details">
+              <div class="user-name">John Doe</div>
+              <div class="user-role">
+              User
+              </div>
+            </div>
+          </div>
+          <ul class="sidebar-menu">
+            <li class="menu-header">Dashboard</li>
+            <li class="active">
+              <a href="#"><i class="fa fa-home"></i><span>Dashboard</span></a>
+            </li>
+
+            <li class="menu-header">Fund Wallet</li>
+            <li class="">
+              <a href="#"><i class="fa fa-credit-card"></i><span>Fund Wallet</span></a>
+            </li>
+
+            <li class="menu-header">Components</li>
+            <li>
+              <a href="#" ><i class="fa fa-anchor"></i><span>Transaction History</span></a>
+             
+            </li>
+            <li>
+              <a href="#" ><i class="fa fa-rocket"></i><span>Voting History</span></a>
+             
+            </li> 
+            <li>
+              <a href="#"><i class="fa fa-user"></i> Edit Profile</a>
+            </li>
+
+            <li>
+              <a href="#"><i class="fa fa-power-off"></i> Logout</a>
+            </li>
+        </ul>
+          <div class="p-3 mt-4 mb-4">
+            <a href="#" class="btn btn-danger btn-shadow btn-round has-icon has-icon-nofloat btn-block">
+              <i class="ion ion-help-buoy"></i> <div>Become a contestant</div>
+            </a>
+          </div>
+        </aside>
+    </div>
